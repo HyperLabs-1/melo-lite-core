1240bd41b0960cc3203096d64ac40f224a51066a
Aditya <adityadav9532@gmail.com>
2026-08-22 23:23:54 +0530

    Fix github release update url to point to Echo-Music repo

--- service/kotlinYtmusicScraper/src/commonMain/kotlin/echo/music/iad1tya/kotlinytmusicscraper/Ytmusic.kt
+++ service/kotlinYtmusicScraper/src/commonMain/kotlin/echo/music/iad1tya/kotlinytmusicscraper/Ytmusic.kt
@@ -598,7 +598,7 @@ class Ytmusic {
         }
 
     suspend fun checkForGithubReleaseUpdate() =
-        httpClient.get("https://api.github.com/repos/maxrave-dev/SimpMusic/releases/latest") {
+        httpClient.get("https://api.github.com/repos/EchoMusicApp/Echo-Music/releases/latest") {
             contentType(ContentType.Application.Json)
         }
 
